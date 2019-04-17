45270fc3c5cb8ce1b8ef8134b5fe827e6079c403
ZHuTOdy <32008256+ZHuTOdy@users.noreply.github.com>
2019-04-17 19:35:32 +0800

    Create 11051.js

---
+++ univ/11051.js
@@ -0,0 +1,17 @@
+var univ_list = {
+    school: "长江大学",                        // 学校名称
+    id: "11051",                                     // 学校 ID
+    link: [ {
+        name: "长大主页",                            // 链接名称少于8个汉字或其等宽字符
+        url: "http://www.yangtzeu.edu.cn/"          // 链接地址
+    }, {
+        name: "图书馆",
+        url: "http://lib.yangtzeu.edu.cn/"
+    }, {
+        name: "教学管理信息系统",
+        url: "http://221.233.24.23/eams/login.action"
+    }, {
+        name: "教务处",
+        url: "http://jwc.yangtzeu.edu.cn/"
+    }, ]
+};
